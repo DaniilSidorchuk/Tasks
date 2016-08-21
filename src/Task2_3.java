436a396e74f6dff8e68f5b9400f4b47b902fdd0e
Daniil Sidorchuk <daniilsidorchuk@gmail.com>
2016-08-21 20:20:10 +0300

    Task2_3

---
+++ src/Task2_3.java
@@ -0,0 +1,13 @@
+/**
+ - Создайте программу, выводящую на экран все неотрицательные элементы последовательности 90 85 80 75 70 65 60 ….
+ */
+public class Task2_3 {
+    public static void main(String[] args) {
+        int number = 90;
+    while(number>=0){
+        System.out.println(number);
+        number = number - 5;
+
+    }
+    }
+}
