d06f736e4cf79e354a82f3ab5192df787c727fd6
Daniil Sidorchuk <daniilsidorchuk@gmail.com>
2016-08-21 20:13:29 +0300

    Task2_1

---
+++ src/Task2_1.java
@@ -0,0 +1,14 @@
+/**
+ Создайте программу, выводящую на экран все четырёхзначные числа последовательности 1000 1003 1006 1009 1012 1015 …
+ */
+public class Task2_1 {
+    public static void main(String[] args) {
+        int number = 1000;
+        while (number < 10000){
+            System.out.println(number);
+            number++;
+        }
+
+
+    }
+}
