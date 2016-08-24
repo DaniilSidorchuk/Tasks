bcb0b072c2d4282c7c022be2268c7b90dac54bb7
Daniil Sidorchuk <daniilsidorchuk@gmail.com>
2016-08-24 21:32:51 +0300

    Rewrite and fully done Task2_7

--- src/Task2_7.java
+++ src/Task2_7.java
@@ -6,26 +6,45 @@ public class Task2_7 {
     static int[] x = {8, 7, 12, 15, 9, 2, 3, 69, 8, 12, 88, 1, 2000, 0};
     public static void main(String[] args) {
         int y = 0;
-        boolean test = test(x[i]);
+        boolean test1 = test1(x[i]);
+        boolean test2 = test2(x[i]);
 
         while ( i < x.length){
-            if (test(x[i]) == true){
+            if (test1(x[i]) == true){
                 y = y + x[i];
                 i++;
-                if (i == x.length - 1){
-                    System.out.println("Total amount of all even numbers is " + y);
-                    break;
-                }
+
             }
 else i++;
+            if (i == x.length - 1){
+                System.out.println("Total amount of all even numbers is " + y);
+                break;
+            }
+        }
+        y=0;
+        i=0;
+        while ( i < x.length){
+            if (test2(x[i]) == true){
+                y = y + x[i];
+                i++;
+            }
+            else i++;
+            if (i == x.length - 1){
+                System.out.println("Total amount of all odd numbers is " + y);
+                break;
+            }
         }
     }
 
-    public static boolean test (int d ){
+    public static boolean test1 (int d ){
         int test = x[i]%2;
         boolean test2 = test ==0;
         return test2;
     }
-
+    public static boolean test2 (int d ){
+        int test = x[i]%2;
+        boolean test2 = test != 0;
+        return test2;
+    }
     }
 
